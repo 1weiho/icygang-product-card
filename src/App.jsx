8a5befc3da7e3895366836944cec18d9031594cf
David Ho <davidho0403@gmail.com>
2023-01-24 03:03:24 +0800

    feat: generate image preview

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react"
 import html2canvas from "html2canvas"
 
 const MyComponent = () => {
-  // const [imgPreview, setImgPreview] = useState("")
+  const [imgPreview, setImgPreview] = useState("")
   const divRef = useRef(null)
 
   const handleExport = () => {
@@ -10,18 +10,20 @@ const MyComponent = () => {
       backgroundColor: null,
     }).then((canvas) => {
       const dataURL = canvas.toDataURL()
-      // setImgPreview(dataURL)
-      const link = document.createElement("a")
-      link.download = "watermark.png"
-      link.href = dataURL
-      link.click()
+      setImgPreview(dataURL)
+      // const link = document.createElement("a")
+      // link.download = "watermark.png"
+      // link.href = dataURL
+      // link.click()
     })
   }
 
   return (
     <>
       <div className="h-screen w-screen bg-slate-800 flex flex-col justify-center items-center space-y-8">
-        {/* <img src={imgPreview} alt="preview" className="w-96 h-20" /> */}
+        <div className="p-3 border-gray-400 border-2 border-dashed rounded-lg w-96 h-28 flex justify-center items-center">
+          {imgPreview && <img src={imgPreview} alt="preview" className="max-h-full" />}
+        </div>
         <div ref={divRef}>
           <div className="bg-white px-5 py-3 rounded-lg flex w-96 h-20 items-center space-x-4">
             <img src="test-logo.png" alt="Product" className="h-full" />
@@ -29,7 +31,7 @@ const MyComponent = () => {
           </div>
         </div>
         <button onClick={handleExport} className="border-white border-2 rounded-lg px-3 py-2 text-white">
-          下載
+          產生圖片
         </button>
       </div>
     </>
