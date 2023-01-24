53f4bf83c91e95b17fd0818a3bc8ffd3799697c6
David Ho <davidho0403@gmail.com>
2023-01-24 08:44:47 +0800

    feat: improve ux

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import html2canvas from "html2canvas"
 const MyComponent = () => {
   const [imageUrl, setImageUrl] = useState("")
   const [canvasPreview, setCanvasPreview] = useState("")
+  const [inputText, setInputText] = useState("⌨️ 點擊以編輯")
   const divRef = useRef(null)
   const inputFileRef = useRef(null)
 
@@ -28,21 +29,30 @@ const MyComponent = () => {
     <>
       <div className="h-screen w-screen bg-slate-800 flex flex-col justify-center items-center space-y-8">
         <div className="p-3 border-gray-400 border-2 border-dashed rounded-lg w-96 h-28 flex justify-center items-center">
-          {canvasPreview && <img src={canvasPreview} alt="preview" className="max-h-full" />}
+          {canvasPreview ? (
+            <img src={canvasPreview} alt="preview" className="max-h-full" />
+          ) : (
+            <span className="text-gray-400 text-sm tracking-widest">圖片預覽區</span>
+          )}
         </div>
         <div ref={divRef}>
           <div className="bg-white px-5 py-3 rounded-lg flex w-96 h-20 items-center space-x-4">
             <input type="file" hidden ref={inputFileRef} accept="image/*" onChange={onImageChange} />
             <img
-              src={imageUrl ? imageUrl : "test-logo.png"}
+              src={imageUrl ? imageUrl : process.env.PUBLIC_URL + "/image/upload.png"}
               alt="Product"
               className="h-full"
               onClick={onImageUploadBtnClick}
             />
-            <span contentEditable="true">icygang logo</span>
+            <span contentEditable="true" className="tracking-widest overflow-hidden" onClick={() => setInputText("")}>
+              {inputText}
+            </span>
           </div>
         </div>
-        <button onClick={handleExport} className="border-white border-2 rounded-lg px-3 py-2 text-white">
+        <button
+          onClick={handleExport}
+          className="border-white border-2 rounded-lg px-3 py-2 text-white tracking-widest hover:bg-white hover:text-black duration-300"
+        >
           產生圖片
         </button>
       </div>
